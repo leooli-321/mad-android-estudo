ab09ea177ec65968f2033e417c41d0f71b2a5e7d
Leo Oli <111819459+leooli-321@users.noreply.github.com>
2024-08-12 20:26:09 -0300

    Add files via upload

---
+++ apresente-se.kt
@@ -0,0 +1,11 @@
+//Comentario rs
+
+fun main(){
+    
+    var idade = 28 //Minha idade
+    var altura = 1.69f //Altura = '1,numero do amor' (f pois é float)
+    var inicial = 'L' //Inicial do meu nome
+
+    //Impressão das informações do indivídio usando interpolação de strings
+    println("Minha inicial eh $inicial, tenho $idade anos e minha altura eh %.2f".format(altura))
+}
